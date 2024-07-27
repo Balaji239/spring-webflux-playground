a823e05b9abe8eefa5769469241da44a92363220
Balaji <baji2391999@gmail.com>
2024-07-27 12:43:07 +0530

    proj setup

---
+++ src/main/java/com/webfluxplayground/traditional_vs_reactive_api/TraditionalWebController.java
@@ -0,0 +1,4 @@
+package com.webfluxplayground.traditional_vs_reactive_api;
+
+public class TraditionalWebController {
+}
---
+++ src/test/java/com/webfluxplayground/SpringWebfluxPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webfluxplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
